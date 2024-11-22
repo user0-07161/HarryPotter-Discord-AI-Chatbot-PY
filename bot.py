c3f11aef10fd2b5cc8a5c0a3452a9610dae459fd
Your Name <you@example.com>
2024-11-22 21:51:32 +0100

    some rewrite

--- bot.py
+++ bot.py
@@ -6,149 +6,291 @@ import requests
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-import logging
-import random
 import re
-logger = logging.getLogger('discord') # set up the logger
+from discord import app_commands
 
 load_dotenv()
 
-# Hugging Face API endpoint
+channels = []
+huggingface_token = os.getenv('HUGGINGFACE_TOKEN')
 API_URL = 'https://api-inference.huggingface.co/models/Ninja5000/DialoGPT-medium-TWEWYJoshua'
-# API_URL = 'https://api-inference.huggingface.co/models/KaydenSou/Joshua' # many diff responses
-# API_URL = 'https://api-inference.huggingface.co/models/nabarun/DialoGPT-small-joshua' # kinda like og josh, but more out of context
-# API_URL = 'https://api-inference.huggingface.co/models/'
-
-# discord intents
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix="joshua!", intents=intents)
+cooldown = {}
 
-# set up the bot
-class MyBot(commands.AutoShardedBot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.api_endpoint = API_URL
-        self.last_message_time = {}
-        huggingface_token = os.getenv('HUGGINGFACE_TOKEN') # Make sure to set this in your .env file
-        self.request_headers = {
-            'Authorization': 'Bearer {}'.format(huggingface_token),
-            "x-use-cache": "false"
-        }
-    
-bot = MyBot(command_prefix="joshua!", intents=intents, owner_id=707170199861854209) # replace with your discord id
+def EmbedBuilder(type: str, title: str, description: str, fields):
+    embed = discord.Embed()
+    embed.title = title
+    embed.description = description
+    if type == "error":
+        embed.color = discord.Color.red()
+    elif type == "info":
+        embed.color = discord.Color.blurple()
+    else:
+        raise ValueError("type must be either \"info\" or \"error\"")
+    if fields != None:
+        for field in fields:
+            embed.add_field(value = fields[index(field)]["value"], name = fields[index(field)]["name"], inline = fields[index(field)]["inline"])
+    return embed
 
-def query(payload):
+def LoadChannels():
+    global channels
+    try:
+        with open("channels.json", "r") as f:
+            channels = json.load(f)
+    except:
+        pass
+
+def UpdateChannels():
+    global channels
+    try:
+        with open("channels.json", "w") as f:
+            json.dump(channels, f)
+    except:
+        pass
+
+def query(prompt):
     """
-    make request to the Hugging Face model API with exponential backoff retry strategy
+    Get the response from the Hugging Face API
     """
-    # logger.debug(payload)
-    
-    retries = 3
-    backoff_time = 2  # initial backoff time
-    for _ in range(retries):
-        response = requests.post(bot.api_endpoint,
-                                    headers=bot.request_headers,
-                                    json=payload)
+
+    payload = {'inputs': f'You say: {prompt}\nI reply:'}
+    backoff = 3
+
+    for i in range(3):
+        response = requests.post(
+            API_URL, 
+            headers = {
+                'Authorization': f'Bearer {huggingface_token}',
+                "x-use-cache": "false"
+            },
+            json = payload
+        )
         if response.status_code == 429:
-            logger.warning("Received 429 error, backing off for {} seconds".format(backoff_time))
-            time.sleep(backoff_time)
-            backoff_time *= 2  # exponential backoff
+            print("Error 429 received. Backing off...")
+            time.sleep(backoff)
+            backoff *= 2
         else:
-            logger.debug(response)
             return response
-    return None  # if all retries fail
+    return f"ERROR"
 
 @bot.event
 async def on_ready():
-    # logging info when the bot wakes up 
-    logger.info('Logged in as')
-    logger.info(bot.user.name)
-    logger.info(bot.user.id)
-    logger.info(f'Connected to {len(bot.guilds)} guilds')
-    logger.info('------')
-    # send a request to the model without caring about the response
-    # just so that the model wakes up and starts loading
-    # query({'inputs': 'Hello!'})
-    await keep_alive()
+    LoadChannels()
+    await bot.tree.sync()
+    print("Logged in and synced commands.")
 
-# Make a request to the model every 10 minutes to keep it alive
-async def keep_alive():
-    while True:
-        # various inputs so model loads and doesn't cache response
-        inputs = ["yoo!", "wsg?", "wsp potato", "cheesecake macaroni", "potatoes", "lalala", "jajaja", "brr", "mbmb", "abcd", "xyz"]
-        # send a request to the model without caring about the response
-        query({'inputs': random.choice(inputs)})
-        logger.info('Sent keep-alive request to the model')
-        await asyncio.sleep(60 * 10)
+
+@bot.hybrid_command(name = "setup", description = "Set the channel for Joshua")
+@commands.has_permissions(administrator=True)
+async def setup(ctx):
+    message = await ctx.reply(
+        embed=EmbedBuilder(
+            type="info", 
+            title="⏳ Setting channel...", 
+            description="Please wait while the channel is being set", 
+            fields=None
+        )
+    )
+    try:
+        channels.append(str(ctx.channel.id))
+        UpdateChannels()
+        await message.edit(
+            embed=EmbedBuilder(
+                type="info", 
+                title="✅ Channel successfully set!", 
+                description="", fields=None
+            )
+        )
+    except:
+        await message.edit(
+            embed=EmbedBuilder(
+                type="error", 
+                title="❌ Channel couldn't be set.", 
+                description="Please contact **user0_07161**", 
+                fields=None
+            )
+        )
+
+@bot.hybrid_command(name = "unset", description = "Unset the channel for Joshua")
+@commands.has_permissions(administrator=True)
+async def unset(ctx):
+    message = await ctx.reply(
+        embed=EmbedBuilder(
+            type="info", 
+            title="⏳ Unsetting channel...", 
+            description="Please wait while the channel is being set", 
+            fields=None
+        )
+    )
+    try:
+        channels.remove(str(ctx.channel.id))
+        UpdateChannels()
+        await message.edit(
+            embed=EmbedBuilder(
+                type="info", 
+                title="✅ Channel successfully unset!", 
+                description="", 
+                fields=None
+            )
+        )
+    except:
+        await message.edit(
+            embed=EmbedBuilder(
+                type="error", 
+                title="❌ Channel couldn't be unset.", 
+                description="Make sure the channel was set before or contact **user0_07161**", 
+                fields=None
+            )
+        )
 
 @bot.event
 async def on_message(message):
-    """
-    this function is called whenever the bot sees a message in a channel
-    """
-
-    # ignore the message if it comes from the bot itself or a direct message
-    if message.author.id == bot.user.id or isinstance(
-            message.channel, discord.DMChannel):
+    if message.content.startswith(bot.command_prefix):
+        await bot.process_commands(message)
         return
-    if message.author.bot:
+    if (not bot.user.mentioned_in(message) and 
+        not str(message.channel.id) in channels and 
+        not ("@everyone" in message.mentions or 
+            "@here" in message.mentions) and not 
+        isinstance(message.channel, discord.channel.DMChannel) or
+        message.author.bot
+    ):
         return
-    if (message.channel.id not in [946035894601797643, 1296840483229798460, 1297140878917242921, 1296154358832041995, 1297963659691298917, 1302878387710328884]) \
-        and not (bot.user.mentioned_in(message) and not (["@everyone", "@here"] in message.mentions)):
-        return
-
+    try:
+        if time.time() - cooldown[message.author.id] < 5:
+            await message.reply(
+                embed=EmbedBuilder(
+                    type="info", 
+                    title="❄️ Cooldown", 
+                    description=f"-# Please wait `{round(3-(cooldown[message.author.id]-time.time()))}` seconds for the cooldown to finish.",
+                    fields=None
+                )
+            )
+            return
+        else:
+            cooldown[message.author.id] = time.time()
+    except:
+        cooldown[message.author.id] = time.time()
     for match in re.findall(r'<@?!?(\d{11,})>', message.content):
         user = await bot.fetch_user(int(match))
         if user:
             message.content = message.content.replace(f'<@{match}>', f'@{user.name}')
-
-    logger.info(f"Received message from {message.author.name} [{message.author.id}] in {message.guild.name}: {message.content}")
-
-    if message.author.id in bot.last_message_time:
-        if time.time() - bot.last_message_time[message.author.id] < 3:
-            remaining_time = round(3 - (time.time() - bot.last_message_time[message.author.id]))
-            await message.reply(f"Please wait for {remaining_time} seconds for the cooldown to finish.", mention_author=False)
-            return
-    bot.last_message_time[message.author.id] = time.time()
-
-    # form query payload with the content of the message
-    payload = {'inputs': f'You say: {message.content}\nI reply:'}
-
-    # while the bot is waiting on a response from the model
-    # set the its status as typing for user-friendliness
     async with message.channel.typing():
-        max_retries = 3
-        for i in range(max_retries):
-            logger.info(f"Try {str(i + 1)} of {str(max_retries)}")
-            response = query(payload)
-            if response.status_code != 200:
-                if "[503]" in str(response):
-                    logger.error(f"Error [503]: {response.text}")
-                    bot_response = f'`Model Ninja5000/DialoGPT-medium-TWEWYJoshua is currently loading`'
-                else:
-                    logger.error(f"Error occurred: {response.text}")
-                    bot_response = '`Hmm... something is not right.`'
-                continue
-            else:
-                response_data = response.json()
-                bot_response = response_data[0]['generated_text']
-                # remove the prompt from the bot's response
-                bot_response = bot_response[len(payload['inputs']):]
-                if len(bot_response.strip()) > 1:
-                    break
-
-
-    # we may get ill-formed response if the model hasn't fully loaded
-    # or has timed out
-    if not bot_response:
-        if 'error' in response or 'error' in response.text:
-            logger.error(f'`Unkown error: [{response.status_code}] {response.text}`')
-            bot_response = 'An unkown error occured.'
+        response = query(message.content)
+        if "str" in str(type(response)):
+            error = response.split("-")[-1]
+            await message.reply(
+                embed=EmbedBuilder(
+                    type="error",
+                    title="❗ Error",
+                    description=f"An error occured.\nPlease contact **user0_07161**, if this error persists.",
+                    fields=None
+                )
+            )
+            return
+        elif response.status_code == 503:
+            await message.reply(
+                embed=EmbedBuilder(
+                    type="info",
+                    title="⌛ Model loading",
+                    description=f"The model is currently loading...\nPlease wait...",
+                    fields=None
+                )
+            )
+            return
+        elif response.status_code != 200:
+            await message.reply(
+                embed=EmbedBuilder(
+                    type="error",
+                    title="❗ Error",
+                    description=f"An error occured.\nStatus code: {response.status_code}.\nPlease contact **user0_07161**, if this error persists.",
+                    fields=None
+                )
+            )
+            return
         else:
-            logger.error(f"Error occurred: [{response.status_code}] {response.text}")
-            bot_response = '`No response from the model due to unknown reasons.`'
+            try:
+                await message.reply(response.json()[0]['generated_text'].split("I reply:")[-1])
+            except discord.errors.HTTPException:
+                await message.reply(
+                    embed=EmbedBuilder(
+                        type="error",
+                        title="❗ Error",
+                        description=f"An error occured.\nThe model returned empty output.",
+                        fields=None
+                    )
+                )
+            return
+        await message.reply(
+            embed=EmbedBuilder(
+                type="error",
+                title="❗ Error",
+                description=f"An unknown error occured.\nPlease contact **user0_07161**, if this error persists.",
+                fields=None
+            )
+        )
 
-    await message.reply(bot_response.strip(), mention_author=False)
-    logger.info(f"Sent message in {message.guild.name} [{message.guild.id}]: {bot_response.strip()}\n")
+@app_commands.allowed_installs(guilds=True, users=True)
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@bot.tree.command(name="generate", description="Generate a response...")  
+async def generate(interaction, prompt: str):
+    await interaction.response.defer()
+    response = query(prompt)
+    if "str" in str(type(response)):
+        error = response.split("-")[-1]
+        await interaction.edit_original_response(
+            embed=EmbedBuilder(
+                type="error",
+                title="❗ Error",
+                description=f"An error occured.\nPlease contact **user0_07161**, if this error persists.",
+                fields=None
+            )
+        )
+        return
+    elif response.status_code == 503:
+        await interaction.edit_original_response(
+            embed=EmbedBuilder(
+                type="info",
+                title="⌛ Model loading",
+                description=f"The model is currently loading...\nPlease wait...",
+                fields=None
+            )
+        )
+        return
+    elif response.status_code != 200:
+        await interaction.edit_original_response(
+            embed=EmbedBuilder(
+                type="error",
+                title="❗ Error",
+                description=f"An error occured.\nStatus code: {response.status_code}.\nPlease contact **user0_07161**, if this error persists.",
+                fields=None
+            )
+        )
+        return
+    else:
+        try:
+            await interaction.edit_original_response(
+                content=response.json()[0]['generated_text'].split("I reply:")[-1]
+            )
+        except discord.errors.HTTPException:
+            await interaction.edit_original_response(
+                embed=EmbedBuilder(
+                    type="error",
+                    title="❗ Error",
+                    description=f"An error occured.\nThe model returned empty output.",
+                    fields=None
+                )
+            )
+        return
+    await interaction.edit_original_response(
+        embed=EmbedBuilder(
+            type="error",
+            title="❗ Error",
+            description=f"An unknown error occured.\nPlease contact **user0_07161**, if this error persists.",
+            fields=None
+        )
+    )
 
 bot.run(os.getenv('DISCORD_TOKEN'))
